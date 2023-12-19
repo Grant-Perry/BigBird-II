0a0d58dc17543a835c4212a3d999c382fa01c9d4
Grant Perry <grant@grantperry.com>
2023-12-19 12:20:22 -0500

    Initial Commit

---
+++ BigBird/BigBirdApp.swift
@@ -0,0 +1,19 @@
+//   BigBirdApp.swift
+//   BigBird
+//
+//   Created by: Grant Perry on 12/19/23 at 12:20 PM
+//     Modified: 
+//
+//  Copyright © 2023 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+@main
+struct BigBirdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BigBird/ContentView.swift
@@ -0,0 +1,26 @@
+//   ContentView.swift
+//   BigBird
+//
+//   Created by: Grant Perry on 12/19/23 at 12:20 PM
+//     Modified: 
+//
+//  Copyright © 2023 Delicious Studios, LLC. - Grant Perry
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
